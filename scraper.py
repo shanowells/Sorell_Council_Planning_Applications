d1bf112e9641f93ce3304181c2ebb37c73e1ddc9
shanowells <155600856+shanowells@users.noreply.github.com>
2024-01-04 16:50:22 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,76 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import os
+os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import re
+import scraperwiki
+import logging
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+base_url = 'https://www.sorell.tas.gov.au/currently-advertised-planning-applications/'
+public_notices_url = base_url + '?r=P1.LCC.WEBGUEST&f=%24P1.ESB.PUBNOTAL.ENQ'
+public_notice_details_url = base_url + '?r=P1.LCC.WEBGUEST&f=%24P1.ESB.PUBNOT.VIW&ApplicationId='
+page = BeautifulSoup(scraperwiki.scrape(https://www.sorell.tas.gov.au/currently-advertised-planning-applications/), 'html.parser')
+
+records = []
+for table in page.find_all('table', class_='grid'):
+    record = {
+        'date_scraped': date.today().isoformat()
+    }
+    for tr in table.find_all('tr'):
+        header = tr.find('td', class_="headerColumn").get_text()
+        if not  header:
+            continue
+        element = tr.find('td', class_="headerColumn").find_next_sibling("td")
+
+        if header == 'Application ID':
+            record['Application Number'] = element.find('a').get_text()
+            record['info_url'] = public_notice_details_url + record['council_reference']
+        elif header == 'Application Description':
+            record['Description'] = element.get_text()
+        elif header == 'Property Address':
+            record['address'] = re.sub(r'\sTAS\s+(7\d{3})$', r', TAS, \1', element.get_text())
+
+    records.append(record)
+
+log.info(f"Found {len(records)} public notices")
+
+for record in records:
+    try:
+        rs = scraperwiki.sqlite.select("* from data where council_reference=?", (record['council_reference'],))
+        if rs:
+            continue
+    except Exception as e:
+        if not 'no such table' in str(e): # happens on very first record only
+            raise e
+
+    log.info(f"Scraping Public Notice - Application Details for {record['council_reference']}")
+    page = BeautifulSoup(scraperwiki.scrape(record['info_url']), 'html.parser')
+
+    for table in page.find_all('table', class_='grid'):
+        for tr in table.find_all('tr'):
+            try:
+                header_element = tr.find('td', class_="headerColumn")
+                if not header_element:
+                    continue
+                header = header_element.get_text()
+                value = tr.find('td', class_="headerColumn").find_next_sibling("td").get_text()
+                if value == '\xa0':
+                    # empty cell containing only &nbsp;
+                    continue
+                elif header == "Property Legal Description":
+                    record['address'] = value
+                elif header == "Application Received":
+                    record['date_received'] = datetime.strptime(value, '%d/%m/%Y').date().isoformat()
+                elif header == "Advertised On":
+                    record['on_notice_from'] = datetime.strptime(value, '%d/%m/%Y').date().isoformat()
+                elif header == "Advertised Close":
+                    record['closes'] = datetime.strptime(value, '%d/%m/%Y').date().isoformat()
+            except Exception as e:
+                raise e
+
+    scraperwiki.sqlite.save(unique_keys=['council_reference'], data=record, table_name="data")
